9c62f25b1fc0c7dba6008c59da8cf1d6b2f3200d
Robert Gruberski <rgruberski@gmail.com>
2024-08-27 14:58:18 +0200

    [Sprint 3][Task 4] fix: implement verify-token route

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -1,5 +1,21 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Deserialize;
+use crate::domain::{AuthAPIError};
+use crate::utils::auth;
 
-pub async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+pub async fn verify_token(
+    Json(request): Json<VerifyTokenRequest>
+) -> Result<impl IntoResponse, AuthAPIError> {
+
+    match auth::validate_token(&request.token).await {
+        Ok(_) => (),
+        Err(_) => return Err(AuthAPIError::InvalidToken),
+    }
+
+    Ok(StatusCode::OK.into_response())
+}
+
+#[derive(Deserialize)]
+pub struct VerifyTokenRequest {
+    pub token: String
 }
--- auth-service/tests/api/verify_token.rs
+++ auth-service/tests/api/verify_token.rs
@@ -1,10 +1,83 @@
-use crate::helpers::TestApp;
+use auth_service::utils::constants::JWT_COOKIE_NAME;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn verify_token_returns_200() {
+async fn should_return_422_if_malformed_input() {
+
     let app = TestApp::new().await;
 
-    let response = app.post_verify_token().await;
+    let test_cases = [
+        serde_json::json!({
+            "email": get_random_email(),
+            "loginAttemptId": "example string"
+        }),
+        serde_json::json!({
+            "email": get_random_email(),
+            "2FACode": "example string"
+        }),
+        serde_json::json!({
+            "loginAttemptId": "example string",
+            "2FACode": "example string"
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_verify_token(&test_case).await;
+
+        assert_eq!(response.status().as_u16(), 422);
+    }
+}
+
+#[tokio::test]
+async fn should_return_200_valid_token() {
+
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": false
+    });
+
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+    });
+
+    let response = app.post_login(&login_body).await;
 
     assert_eq!(response.status().as_u16(), 200);
+
+    let cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
+        .expect("Auth cookie not found");
+
+    let verify_token_body = serde_json::json!({
+        "token": cookie.value(),
+    });
+
+    let response = app.post_verify_token(&verify_token_body).await;
+
+    assert_eq!(response.status(), 200);
+}
+
+#[tokio::test]
+async fn should_return_401_if_invalid_token() {
+
+    let app = TestApp::new().await;
+
+    let verify_token_body = serde_json::json!({
+        "token": "invalid token",
+    });
+
+    let response = app.post_verify_token(&verify_token_body).await;
+
+    assert_eq!(response.status(), 401);
 }
